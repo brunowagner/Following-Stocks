d5d1e78ee30dad784f5f660a96808cefeb5a0248
Bruno Wagner <brunowvmoraes@gmail.com>
2018-09-16 14:21:15 -0300

    Initial commit.

---
+++ FollowingStocks/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FollowingStocks
+//
+//  Created by Bruno W on 16/09/2018.
+//  Copyright © 2018 Bruno_W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
