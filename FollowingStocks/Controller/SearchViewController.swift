0d45acccd06c82ae30e538b48cc7fb5af7d76feb
Bruno Wagner <brunowvmoraes@gmail.com>
2018-09-17 21:47:00 -0300

    Add HTTPTools on project
    HTTPTools was created to "On The Map" and used in VirtualTourist too.

---
+++ FollowingStocks/Controller/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  FollowingStocks
+//
+//  Created by Bruno W on 16/09/2018.
+//  Copyright © 2018 Bruno_W. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class SearchViewController : UITableViewController {
+    
+}
