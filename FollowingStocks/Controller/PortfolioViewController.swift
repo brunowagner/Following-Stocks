0f58bdbf4d995ed3f309224a814bfc222b1b4ad9
Bruno Wagner <brunowvmoraes@gmail.com>
2018-09-24 11:34:51 -0300

    Implements fetch in PortfolioViewController.
    implementes tableviewSource too.

--- FollowingStocks/Controller/PortfolioViewController.swift
+++ FollowingStocks/Controller/PortfolioViewController.swift
@@ -6,14 +6,46 @@
 //  Copyright © 2018 Bruno_W. All rights reserved.
 //
 
+/*
+    Lógica:
+ 
+ > ao iniciar:
+ -      Procurar dados de papeis e cotações persistidos no core data
+ -      carregar a tabela (tableView)
+ 
+ > ao clicar no botão "Add":
+ -      Ir para searchView informado que é uma operação de adição
+ -      Na searchView:
+ -          ao encontrar o clicar no papel:
+ -              exibir janela de input com os campos já preenchidos aguardando apenas a confirmação do usuário
+ -              se o usuário clicar em add:
+ -                  se papel já existe, realizar adição da quantidade e calcular o preco médio
+ -                  se o papel não existe, adicionar os dados do papel além de quantidades e preço
+ -              se o usuário clicar em cancelar:
+ -                  voltar para a view do portifolio.
+ */
+
 import UIKit
+import CoreData
 
 class PortfolioViewController: UIViewController {
+    
+    var fetchedResultsController : NSFetchedResultsController<Paper>!
+    
+    @IBOutlet weak var tableView: UITableView!
 
+
+    //MARK: Life's cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        setupPaperFetchedResultsController()
+        
+        //tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +53,85 @@ class PortfolioViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: Fetcheds
+    
+    fileprivate func setupPaperFetchedResultsController() {
+        let fetchRequest : NSFetchRequest<Paper> = Paper.fetchRequest()
+        
+        let predicate = NSPredicate(format: "isPortfolio = %@", "true")
+        
+        let sortDescriptor = NSSortDescriptor(key: "symbol", ascending: true)
+        
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: DataController.sharedInstance().viewContext, sectionNameKeyPath: nil, cacheName: "paperFRC")
+        
+        fetchedResultsController.delegate = self
+        
+        do{
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Can not to do fetchedResultsController.performFetch!")
+        }
     }
-    */
+}
 
+
+    // MARK: UITableViewDataSource
+extension PortfolioViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PortfolioCell") as! PortfolioCell
+        
+        let paper = fetchedResultsController.object(at: indexPath)
+        
+        cell.symbol.text = paper.symbol
+        cell.exchange.text = paper.exchange
+        cell.price.text = "\(paper.quote?.price ?? 0)"
+        cell.change.text = "\(paper.quote?.change ?? 0)"
+        
+        return cell
+    }
+}
+
+
+
+    // MARK: - NSFetchedResultsControllerDelegate
+extension PortfolioViewController: NSFetchedResultsControllerDelegate{
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [indexPath!], with: .fade)
+            break
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+            break
+        case .update:
+            tableView.reloadData()
+            break
+        case .move:
+                //move isn't applied in this app
+            break
+        }
+    }
 }
---
+++ FollowingStocks/Model/DataController.swift
@@ -0,0 +1,81 @@
+//
+//  DataController.swift
+//  VirtualTourist
+//
+//  Created by Bruno W on 23/08/2018.
+//  Copyright © 2018 Bruno_W. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController{
+    
+    //MARK: Properties
+    var persistentContainer : NSPersistentContainer
+    var viewContext : NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    var backgroundContext: NSManagedObjectContext!
+    
+    //MARK: - Methods
+    private init(modelName : String){
+        self.persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (()->Void)? = nil){
+        persistentContainer.loadPersistentStores { (storeDiscription, error) in
+            //se houver erro, para o app e lança uma "fatalErro"
+            guard error == nil else{
+                fatalError((error?.localizedDescription)!)
+            }
+            
+            // função criada para configurar os contextos da fila principal e da fila privada(background)
+            self.configureContexts()
+            
+            print ("Data Stak Loadded")
+            completion?()
+        }
+    }
+    
+    private func configureContexts(){
+        
+        //instancia um contexto associado com uma fila privada
+        backgroundContext = persistentContainer.newBackgroundContext()
+        
+        //determina que os contextos pares realizem a fusão de mudanças automaticamente
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        
+        //define as políticas de fusão para que o app não trave em caso de conflito
+        // mergeByPropertyObjectTrump -> prefere os valores das propriedades do objeto em caso de conflito.
+        // mergeByPropertyStoreTrump -> prefere os valores das propriedades do persistentStore em caso de conflito.
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+    }
+    
+    // função para salvamento em intervalos
+    func autoSaveViewContext(interval : TimeInterval = 30){
+        print("autosaving...")
+        guard interval > 0 else{
+            print("Cannot set negative autosave interval")
+            return
+        }
+        // sá salva se houver mudanças no contexto.
+        if viewContext.hasChanges{
+            try? viewContext.save()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+
+
+    // MARK: - Shared Instance
+    class func  sharedInstance() -> DataController{
+        struct Singleton{
+            static let sharedInstance = DataController(modelName: "FollowingStocks")
+        }
+        return Singleton.sharedInstance
+    }
+}
