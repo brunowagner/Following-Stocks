78a0871ca3b85a2b90f598e32947294dbb6107e9
Bruno Wagner <brunowvmoraes@gmail.com>
2018-10-05 20:20:30 -0300

    Implements operationType enum in Trade.

---
+++ FollowingStocks/Model/Extensions/Trade+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Trade+Extensions.swift
+//  FollowingStocks
+//
+//  Created by Bruno W on 05/10/2018.
+//  Copyright © 2018 Bruno_W. All rights reserved.
+//
+
+import Foundation
+extension Trade {
+    enum operationType : String {
+        case purchase
+        case sale
+    }
+}
