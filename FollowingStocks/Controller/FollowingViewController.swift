438e5d7660a897637db7479e28b50a1c6f9c1d25
Bruno Wagner <brunowvmoraes@gmail.com>
2018-11-06 18:13:50 -0200

    Modified PaperViewController.

--- FollowingStocks/Controller/FollowingViewController.swift
+++ FollowingStocks/Controller/FollowingViewController.swift
@@ -9,139 +9,143 @@
 import UIKit
 import CoreData
 
-class FollowingViewController: UIViewController {
+class FollowingViewController: PaperViewController {
     
     //MARK: Properties
-    var fetchedResultsController : NSFetchedResultsController<Paper>!
-    static let limitOfPapers : Int = 5
+    //var fetchedResultsController : NSFetchedResultsController<Paper>!
+    //static let limitOfPapers : Int = 5
     
     //MARK: Outlets
-    @IBOutlet weak var tableView: UITableView!
     
+
     //MARK: Life's cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("\(type(of: self)) - viewDidLoad")
-        tableView.dataSource = self
-        tableView.delegate = self
+//        print("\(type(of: self)) - viewDidLoad")
+//        tableView.dataSource = self
+//        tableView.delegate = self
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("\(type(of: self)) - viewWillAppear")
-        setupPaperFetchedResultsController()
-        tableView.reloadData()
+    override func setPredicate(predicate: NSPredicate) {
+        self.predicate = NSPredicate(format: "isFollowed == %@", NSNumber(value: true))
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("\(type(of: self)) - viewDidDisappear")
-        fetchedResultsController = nil
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        print("\(type(of: self)) - viewWillAppear")
+//        setupPaperFetchedResultsController()
+//        tableView.reloadData()
+//    }
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//        print("\(type(of: self)) - viewDidDisappear")
+//        fetchedResultsController = nil
+//    }
     
-    //MARK: Fetcheds
-    fileprivate func setupPaperFetchedResultsController() {
-        print("Iniciando Fetched...")
-        let fetchRequest : NSFetchRequest<Paper> = Paper.fetchRequest()
-        let predicate = NSPredicate(format: "isFollowed == %@", NSNumber(value: true))
-        let sortDescriptor = NSSortDescriptor(key: "symbol", ascending: true)
-        fetchRequest.predicate = predicate
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: DataController.sharedInstance().viewContext, sectionNameKeyPath: nil, cacheName: nil)
-        fetchedResultsController.delegate = self
-        
-        do{
-            try fetchedResultsController.performFetch()
-        } catch {
-            fatalError("Can not to do fetchedResultsController.performFetch!")
-        }
-    }
-    
-    //MARK: Statics Functions
-    static func getPapersCount() -> Int {
-        let fetchRequest : NSFetchRequest<Paper> = Paper.fetchRequest()
-        let predicate = NSPredicate(format: "isFollowed == %@", NSNumber(value: true))
-        fetchRequest.predicate = predicate
-        
-        if let result = try? DataController.sharedInstance().viewContext.fetch(fetchRequest){
-            return result.count
-        } else {
-            return 0
-        }
-    }
-    
-    static func limitOfPapersReached() -> Bool {
-        let numOfPapers : Int = self.getPapersCount()
-        
-        if numOfPapers >= limitOfPapers {
-            return true
-        } else {
-            return false
-        }
-    }
+//    //MARK: Fetcheds
+//    fileprivate func setupPaperFetchedResultsController() {
+//        print("Iniciando Fetched...")
+//        let fetchRequest : NSFetchRequest<Paper> = Paper.fetchRequest()
+//        let predicate = NSPredicate(format: "isFollowed == %@", NSNumber(value: true))
+//        let sortDescriptor = NSSortDescriptor(key: "symbol", ascending: true)
+//        fetchRequest.predicate = predicate
+//        fetchRequest.sortDescriptors = [sortDescriptor]
+//        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: DataController.sharedInstance().viewContext, sectionNameKeyPath: nil, cacheName: nil)
+//        fetchedResultsController.delegate = self
+//
+//        do{
+//            try fetchedResultsController.performFetch()
+//        } catch {
+//            fatalError("Can not to do fetchedResultsController.performFetch!")
+//        }
+//    }
 }
+//    //MARK: Statics Functions
+//    static func getPapersCount() -> Int {
+//        let fetchRequest : NSFetchRequest<Paper> = Paper.fetchRequest()
+//        let predicate = NSPredicate(format: "isFollowed == %@", NSNumber(value: true))
+//        fetchRequest.predicate = predicate
+//
+//        if let result = try? DataController.sharedInstance().viewContext.fetch(fetchRequest){
+//            return result.count
+//        } else {
+//            return 0
+//        }
+//    }
 
-// MARK: - UITableViewDataSource
-extension FollowingViewController: UITableViewDataSource{
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return fetchedResultsController.sections?.count ?? 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingCell", for: indexPath) as! PortfolioCell
-        let paper = fetchedResultsController.object(at: indexPath)
-        cell.setFieldsBy(paper: paper)
-        return cell
-    }
-}
+//    static func limitOfPapersReached() -> Bool {
+//        let numOfPapers : Int = self.getPapersCount()
+//
+//        if numOfPapers >= limitOfPapers {
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+//}
 
-//MARK: - UITableViewDelegate
-extension FollowingViewController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        tableView.deselectRow(at: indexPath, animated: false)
-        
-        let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController2") as! DetailViewController2
-        detailVC.paper = fetchedResultsController.object(at: indexPath)
-        self.navigationController?.pushViewController(detailVC, animated: true)
-    }
-}
+//// MARK: - UITableViewDataSource
+//extension FollowingViewController: UITableViewDataSource{
+//
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return fetchedResultsController.sections?.count ?? 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingCell", for: indexPath) as! PortfolioCell
+//        let paper = fetchedResultsController.object(at: indexPath)
+//        cell.setFieldsBy(paper: paper)
+//        return cell
+//    }
+//}
 
-// MARK: - NSFetchedResultsControllerDelegate
-extension FollowingViewController: NSFetchedResultsControllerDelegate{
-    
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        
-        switch type {
-        case .insert:
-            print("FRC type = insert")
-            tableView.insertRows(at: [newIndexPath!], with: .fade)
-            break
-        case .delete:
-            print("FRC type = delete")
-            tableView.deleteRows(at: [indexPath!], with: .fade)
-            break
-        case .update:
-            print("FRC type = update")
-            tableView.reloadData()
-            break
-        case .move:
-            print("FRC type = move")
-            //move isn't applied in this app
-            break
-        }
-    }
-}
+////MARK: - UITableViewDelegate
+//extension FollowingViewController: UITableViewDelegate{
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//        tableView.deselectRow(at: indexPath, animated: false)
+//
+//        let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController2") as! DetailViewController2
+//        detailVC.paper = fetchedResultsController.object(at: indexPath)
+//        self.navigationController?.pushViewController(detailVC, animated: true)
+//    }
+//}
+
+//// MARK: - NSFetchedResultsControllerDelegate
+//extension FollowingViewController: NSFetchedResultsControllerDelegate{
+//
+//    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+//        tableView.beginUpdates()
+//    }
+//
+//    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+//        tableView.endUpdates()
+//    }
+//
+//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+//
+//        switch type {
+//        case .insert:
+//            print("FRC type = insert")
+//            tableView.insertRows(at: [newIndexPath!], with: .fade)
+//            break
+//        case .delete:
+//            print("FRC type = delete")
+//            tableView.deleteRows(at: [indexPath!], with: .fade)
+//            break
+//        case .update:
+//            print("FRC type = update")
+//            tableView.reloadData()
+//            break
+//        case .move:
+//            print("FRC type = move")
+//            //move isn't applied in this app
+//            break
+//        }
+//    }
+//}
---
+++ FollowingStocks/Controller/PaperViewController.swift
@@ -0,0 +1,153 @@
+//
+//  PaperViewController.swift
+//  FollowingStocks
+//
+//  Created by Bruno W on 01/11/2018.
+//  Copyright © 2018 Bruno_W. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class PaperViewController: UIViewController {
+    
+    //MARK: Properties
+    var fetchedResultsController : NSFetchedResultsController<Paper>!
+    var predicate : NSPredicate!
+    static let limitOfPapers : Int = 5
+    
+    //MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
+    //var tableView : UITableView!
+
+    //MARK: Life's cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("\(type(of: self)) - viewDidLoad")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("\(type(of: self)) - viewWillAppear")
+        setupPaperFetchedResultsController()
+        tableView.reloadData()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("\(type(of: self)) - viewDidDisappear")
+        fetchedResultsController = nil
+    }
+    
+    //MARK: Fetcheds
+    func setPredicate(predicate: NSPredicate) {
+        fatalError("Implements setPredicate")
+    }
+    
+    fileprivate func setupPaperFetchedResultsController() {
+        print("Iniciando Fetched...")
+        let fetchRequest : NSFetchRequest<Paper> = Paper.fetchRequest()
+        //let predicate = NSPredicate(format: "isFollowed == %@", NSNumber(value: true))
+        let sortDescriptor = NSSortDescriptor(key: "symbol", ascending: true)
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: DataController.sharedInstance().viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        
+        do{
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Can not to do fetchedResultsController.performFetch!")
+        }
+    }
+    
+    //MARK: Statics Functions
+    static func getPapersCount() -> Int {
+        let fetchRequest : NSFetchRequest<Paper> = Paper.fetchRequest()
+        let predicate = NSPredicate(format: "isFollowed == %@", NSNumber(value: true))
+        fetchRequest.predicate = predicate
+        
+        if let result = try? DataController.sharedInstance().viewContext.fetch(fetchRequest){
+            return result.count
+        } else {
+            return 0
+        }
+    }
+    
+    static func limitOfPapersReached() -> Bool {
+        let numOfPapers : Int = self.getPapersCount()
+        
+        if numOfPapers >= limitOfPapers {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+// MARK: UITableViewDataSource
+extension PaperViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingCell", for: indexPath) as! PortfolioCell
+        let paper = fetchedResultsController.object(at: indexPath)
+        cell.setFieldsBy(paper: paper)
+        return cell
+    }
+}
+
+//MARK: UITableViewDelegate
+extension PaperViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController2") as! DetailViewController2
+        detailVC.paper = fetchedResultsController.object(at: indexPath)
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
+
+// MARK: - NSFetchedResultsControllerDelegate
+extension PaperViewController: NSFetchedResultsControllerDelegate{
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            print("FRC type = insert")
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+            break
+        case .delete:
+            print("FRC type = delete")
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+            break
+        case .update:
+            print("FRC type = update")
+            tableView.reloadData()
+            break
+        case .move:
+            print("FRC type = move")
+            //move isn't applied in this app
+            break
+        }
+    }
+}
