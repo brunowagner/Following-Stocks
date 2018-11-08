295fd359090e523b15f6721205895f6859e619cc
Bruno Wagner <brunowvmoraes@gmail.com>
2018-11-08 18:54:57 -0200

    Cleanup NSMutableAttributedString.

--- FollowingStocks/View/Extensions/NSMutableAttributedString+Extensions.swift
+++ FollowingStocks/View/Extensions/NSMutableAttributedString+Extensions.swift
@@ -10,19 +10,6 @@
 import Foundation
 import UIKit
 extension NSMutableAttributedString{
-//    func setColorForText(_ textToFind: String?, with color: UIColor) {
-//
-//        let range:NSRange?
-//        if let text = textToFind{
-//            range = self.mutableString.range(of: text, options: .caseInsensitive)
-//        }else{
-//            range = NSMakeRange(0, self.length)
-//        }
-//        if range!.location != NSNotFound {
-//            addAttribute(NSAttributedStringKey.foregroundColor, value: color, range: range!)
-//        }
-//    }
-    
     func setColorForRacionalNumber(positiveColor: UIColor, negativeColor: UIColor) {
         let range = NSMakeRange(0, self.length)
         let color : UIColor
